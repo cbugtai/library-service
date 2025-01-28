4943f79541c6abfd59fffb24d5cd6dbe9baa1243
rrc-F2024WI-cbugtai <150991072+cbugtai@users.noreply.github.com>
2025-01-28 18:50:57 +0000

    feat: updated book model

--- src/models/bookModel.ts
+++ src/models/bookModel.ts
@@ -1,3 +1,9 @@
+export enum BookStatus {
+    AVAILABLE = "Available",
+    BORROWED = "Borrowed",
+    RESERVED = "Reserved",
+}
+
 export interface Book {
     id: string;
     title: string;
@@ -6,4 +12,5 @@ export interface Book {
     isBorrowed: boolean;
     borrowerId?: string;
     dueDate?: string;
+    status: BookStatus;
 }
